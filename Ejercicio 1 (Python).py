861d9c2dd221c45688e8e89af8975316476be63f
Geo1803ml <carlosgeovannyml12@gmail.com>
2025-09-02 06:10:41 +0000

    Ejercicios 1

---
+++ Ejercicio 1 (Python).py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+ # Declaracion 
+numeros = [10, 20, 30]
+vacio = [None] * 5   # lista de tamaño 5
+
+ # Asignacion
+numeros[0] = 99
+
+ # Lectura
+print(numeros[0])  # 99
+
+ # Recorrido
+for n in numeros:
+    print(n)
